5343079f6264047520f71754d117d4dbd8ba807e
Nikhilkathale741 <95696925+Nikhilkathale741@users.noreply.github.com>
2025-06-12 21:54:31 +0530

    Update settings.py

--- PCCM/PCCM/settings.py
+++ PCCM/PCCM/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5+wx-7m+71^iy^c62@-cvc5g9f_v+2i5g3!*jb&2z+!g3eqpro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["51.20.72.207"]
+ALLOWED_HOSTS = ["13.48.71.107"]
 
 
 # Application definition
@@ -122,9 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
